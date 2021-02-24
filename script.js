6160f9fdb37e187774740cb1f7a51199df8b4f12
emilymetzgar93 <emilymetzgar93@gmail.com>
2021-02-23 21:40:02 -0600

    added functions

--- script.js
+++ script.js
@@ -23,24 +23,69 @@
 
 
 //pwLength = window.prompt("How many total characters would you like your password to be?", 'min 8 max 128');
-//if (isNaN(pwLength)){
- // alert("Password length must be provided as a number");
-//}
 
-pwLength = window.prompt("How many total characters would you like your password to be?", 'min 8 max 128');
+
+function getPasswordOptions () {
+
+if (isNaN(pwLength) === true) {
+alert("Password length must be provided as a number");
+return;
+}
+
+var pwLength = window.prompt("How many total characters would you like your password to be?", 'min 8 max 128');
 if (pwLength < 8 || pwLength> 128){
   alert('Password length must be bewtween 8 and 128 characters in length');
+  return;
 }
-else {
-useUppercase = window.confirm("include upper case letters?");
-useLowercase = window.confirm("include lower case letters?");
-useNumbers = window.confirm("include numbers?");
-useSpecial = window.confirm("include special characters?");
-  }
 
+var useUppercase = window.prompt("Incdlue uppercase letters?");
+var useLowercase = window.prompt("Include lowercase letters?");
+var useNumbers = window.prompt("Include numbers?");
+var useSpecial = window.prompt("Include special characters?");
 
 if (!useUppercase && !useLowercase && !useNumbers && !useSpecial){
   alert("User must select at least one character type");
+  return;
 }
 
+var pwOptions = {
+  useUppercase: window.prompt("Incdlue uppercase letters?"),
+  useLowercase: window.prompt("Include lowercase letters?"),
+  useNumners: window.prompt("Include numbers?"),
+  useSpecial: window.prompt("Include special characters?"),
+};
+return pwOptions;
+}
+
+
+var getRandomFunction = {
+  includeLower: getRandomLower,
+  includeUpper: getRandomUpper,
+  includeNumbers: getRandomNumbers,
+  includeSpecial: getRandomSpecial,
+};
+
+function getRandomLower() {
+  var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  return lower[Math.floor(Math.random() * symbols.length)];
+}
+
+function getRandomUpper() {
+  var upper =  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; 
+  return upper[Math.floor(Math.random() * upper.length)];
+}
+
+function getRandomSpecial() {
+  var special = ['!', '@', '#', '$', '%', '^', '&', '*', '~', '?'];
+  return special[Math.floor(Math.random() * special.length)];
+}
+
+function getRandomNumbers () {
+  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+  return numbers[Math.floor(Math.random() *numbers.length)];
+}
+
+function generatePassword() {
+
+}
 
