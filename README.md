536ead53ade7e22151dda66382a652ccf2525fd2
emilymetzgar93 <emilymetzgar93@gmail.com>
2021-02-25 20:09:03 -0600

    updated read me

--- README.md
+++ README.md
@@ -1,92 +1,46 @@
 # 03 JavaScript: Password Generator
 
-## Your Task
+## Description
 
-This week’s homework requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+This application enables users to generate random passwords based on criteria that they’ve selected. This app will run in the browser and uses updated HTML and CSS powered by JavaScript code. It has a responsive user interface that adapts to multiple screen sizes.
 
-The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
+## Installation
 
-## User Story
+Runs in the browser and features HTML, CSS, both powered by Javascript. 
 
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
+## Usage
 
-## Acceptance Criteria
+Organizes data in a readable and accesible format to generate random passwords based off user input. 
 
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
+![A screenshot of the application](./assets/images/screenshot.png)
 
-## Mock-Up
+link to application: https://emilymetzgar.github.io/03-password-generator/
 
-The following image shows the web application's appearance and functionality:
 
-![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
+## Credits
 
-## Grading Requirements
+Emily Metzgar
 
-This homework is graded based on the following criteria: 
+## License
 
-### Technical Acceptance Criteria: 40%
+MIT License
 
-* Satisfies all of the preceding acceptance criteria plus the following:
+Copyright (c) [2021] [Emily Metzgar SMU Coding Bootcamp]
 
-  * The homework should not produce any errors in the console when you inspect it using Chrome DevTools.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-### Deployment: 32%
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the homework instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository, with a unique name and a readme describing the project.
-
-- - -
-© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,49 +1,52 @@
+//arrays to store characters to be randomly accessed
 
-var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upper =  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; 
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var special = ['!', '@', '#', '$', '%', '^', '&', '*', '~', '?'];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
 
-
+//function to prompt user for user input to get password options
 function getPasswordOptions() {
-  
+
   // Variable to store length of password from user input
-  var length =  parseInt(
+  var length = parseInt(
     window.prompt("How many total characters would you like your password to be?"));
-  
+
   // Conditional statement to check if pw length is a number
   if (isNaN(length) === true) {
-  alert("Password length must be provided as a number");
-   return;
+    alert("Password length must be provided as a number");
+    return;
   }
   // Condtional statement to check if password length is at least 8 characters
   if (length < 8) {
     alert('Password length must be at least 8 characters');
     return;
   }
-  
+
   // Condtional statement to check if password length is no more than 128 characters
   if (length > 128) {
     alert('Password length must be less than 129 characters');
     return;
   }
 
+  //confirm messages to prompt user
   var useSpecial = window.confirm("Click OK to include special characters");
   var useNumbers = window.confirm("Click OK to include numbers");
   var useLowercase = window.confirm("Click OK to include lowercase letters");
   var useUppercase = window.confirm("Click OK to include uppercase lettters");
-  
-  var passwordOptions = {
-  length: length,
-  useSpecial: useSpecial,
-  useNumbers: useNumbers,
-  useLowercase: useLowercase,
-  useUppercase: useUppercase,
-  };
 
+  //object to store user input
+  var passwordOptions = {
+    length: length,
+    useSpecial: useSpecial,
+    useNumbers: useNumbers,
+    useLowercase: useLowercase,
+    useUppercase: useUppercase,
+  };
+  //return object
   return passwordOptions;
-  }
-  
+}
+
 
 // Function for getting a random element from an array
 function getRandomElement(characterArrays) {
@@ -52,35 +55,32 @@ function getRandomElement(characterArrays) {
   return randomElement;
 }
 // Function to generate password with user input 
-function generatePassword() { 
-  
+function generatePassword() {
+
   var options = getPasswordOptions();
 
   // Array to store password as it's being concatenated 
-  var result = [] ;
+  var result = [];
 
   // Array to store types of characters to include in password
-  var possibleCharacters = [] ;
- 
+  var possibleCharacters = [];
 
+// Conditional statement that adds array of special characters into array of possible characters based on user input
   if (options.useSpecial) {
     possibleCharacters = possibleCharacters.concat(special);
   }
 
   // Conditional statement that adds array of numbers characters into array of possible characters based on user input
-  // Push new random numbers character to guaranteedCharacters to make at least one of the value is always included 
   if (options.useNumbers) {
-    possibleCharacters = possibleCharacters.concat(numbers); 
+    possibleCharacters = possibleCharacters.concat(numbers);
   }
 
   // Conditional statement that adds array of lowercase characters into array of possible characters based on user input
-  // Push new random lowercase character to guaranteedCharacters to make at least one of the value is always included 
   if (options.useLowercase) {
     possibleCharacters = possibleCharacters.concat(lower);
   }
 
   // Conditional statement that adds array of uppercase characters into array of possible characters based on user input
-  // Push new random uppercase character to guaranteedCharacters to make at least one of the value is always included 
   if (options.useUppercase) {
     possibleCharacters = possibleCharacters.concat(upper);
   }
@@ -98,19 +98,18 @@ function generatePassword() {
 
 // Get references to the #generate element
 
-
 var generateBtn = document.querySelector("#generate");
 
-function writePassword() { 
+function writePassword() {
 
-// Runs the function that will generate the password
-var password = generatePassword();
+  // Runs the function that will generate the password
+  var password = generatePassword();
 
-// Selects on the HTML where the password is shown
-var passwordText= document.querySelector('#password');
-// Makes the value of the element the string generated from the generatePassword function
-passwordText.value = password;
+  // Selects on the HTML where the password is shown
+  var passwordText = document.querySelector('#password');
+  // Makes the value of the element the string generated from the generatePassword function
+  passwordText.value = password;
 }
-
+//add event listener to button 
 generateBtn.addEventListener("click", writePassword);
 
